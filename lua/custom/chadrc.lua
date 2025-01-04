d9749299ba4d604e781cf737bdf797d92443fa54
James <jamesfleming.sd@gmail.com>
2025-01-03 21:34:03 -0800

    Added Custom Files

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,14 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = { 
+    theme = 'tundra',
+    -- transparency = true,
+    nvdash = {
+        load_on_startup = true,
+    },
+}
+
+M.plugins = 'custom.plugins'
+
+return M
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,31 @@
+local plugins = {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+            marksman = {},
+            },
+        }
+    },
+    { "blazkowolf/gruber-darker.nvim" }, 
+    {
+        "2nthony/vitesse.nvim",
+        dependencies = {
+            "tjdevries/colorbuddy.nvim"
+        },
+        build = function()
+            require("vitesse").setup {
+                themes = {
+                    activeBackground = "#F2F0EF"
+                }
+            }
+        end
+    },
+    { "samir-roy/shinjuku.nvim" },
+    { "jbyuki/nabla.nvim" },
+    { "ramojus/mellifluous.nvim" },
+}
+
+-- checking something
+
+return plugins
